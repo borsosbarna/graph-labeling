af8569a6e5983411aebc6079d08e8c71f944cb88
Borsos Barna <borsos.barna96@gmail.com>
2018-05-01 12:41:26 +0300

    Add GA prototype

--- server/controllers/GAController.js
+++ server/controllers/GAController.js
@@ -1,3 +1,206 @@
+const fs = require('fs');
+const { execFile } = require('child_process');
+
+const MAX_RUNNING_TIME = 60; /* (seconds) */
+
 module.exports = (req, res) => {
-    res.send('GAController');
+  /* validate request parameters */
+  /* let errorMsg = validateParameters(req);
+
+  if (errorMsg) {
+    res.json({ errorMsg });
+
+    return;
+  } */
+
+  /* set parameters */
+  const h = req.body.h;
+  const k = req.body.k;
+  const fileContent = req.body.fileContent;
+  const maxLabel = req.body.maxLabel;
+  const populationsCount = req.body.populationsCount;
+  const populationSize = req.body.populationSize;
+  const mutationChance = req.body.mutationChance;
+  const elitesCount = req.body.elitesCount;
+  const maxGenerations = req.body.maxGenerations;
+  const maxTime = req.body.maxTime;
+
+  /* path of the executable file */
+  const execPath = __dirname + '/../bin/GeneticAlgorithm';
+
+  /* create input file */
+  const suffix = Math.floor((Math.random() * 1e6));
+  const filePath = __dirname + '/../tmp/' + suffix + '.in';
+
+  fs.writeFileSync(filePath, fileContent);
+
+  // call exec
+  const child = execFile(execPath,
+    [h, k, filePath, maxLabel, populationsCount, populationSize, mutationChance, elitesCount, maxGenerations, maxTime],
+    (error, stdout, stderr) => {
+    /* check for any error */
+    if (error || stderr) {
+      res.json({ errorMsg: 'Something went wrong!' });
+    }
+    else {
+      const lines = stdout.toString().replace(/\r/g, '').split('\n');
+
+      /* create the JSON object to be returned */
+      const responseJSON = {};
+
+      responseJSON['time'] = parseFloat(lines[0], 10);
+      responseJSON['iterations'] = parseInt(lines[1], 10);
+      responseJSON['solution'] = lines[2].trim();
+      responseJSON['isCorrect'] = lines[3] === '1' ? true : false;
+      responseJSON['conflictingVertexes'] = parseInt(lines[4], 10);
+      responseJSON['chromaticNumber'] = parseInt(lines[5], 10);
+      responseJSON['fitness'] = parseFloat(lines[6], 10);
+      responseJSON['history'] = lines[7];
+      responseJSON['errorMsg'] = null;
+
+      res.json(responseJSON);
+    }
+
+    // delete input file
+    fs.unlink(filePath, () => {});
+  });
 };
+
+function validateParameters(req) {
+  /* extract parameters */
+  const h = req.body.h;
+  const k = req.body.k;
+  const fileContent = req.body.fileContent;
+  const maxLabel = req.body.maxLabel;
+  const temperature = req.body.temperature;
+  const coolingFactor = req.body.coolingFactor;
+  const maxIterations = req.body.maxIterations;
+  const maxTime = req.body.maxTime;
+
+  /* check parameters existance */
+  if (h == null ||
+      k == null ||
+      fileContent == null ||
+      maxLabel == null ||
+      temperature == null ||
+      coolingFactor == null ||
+      maxIterations == null ||
+      maxTime == null) {
+    return 'Please provide all parameters!';
+  }
+
+  /* check parameters type */
+  if (Number.isInteger(Number(h)) == false ||
+      Number.isInteger(Number(k)) == false ||
+      Number.isInteger(Number(maxLabel)) == false ||
+      Number(temperature) == NaN ||
+      Number(coolingFactor) == NaN ||
+      Number.isInteger(Number(maxIterations)) == false ||
+      Number.isInteger(Number(maxTime)) == false) {
+    return 'Please verify parameter types!';
+  }
+
+  /* check parameters value */
+  if (h < 0 ||
+      k < 0 ||
+      maxLabel < 1 ||
+      temperature <= 0 ||
+      coolingFactor <= 0 ||
+      coolingFactor >= 1 ||
+      maxIterations < 1 ||
+      maxTime < 1 ||
+      maxTime > MAX_RUNNING_TIME) {
+    return 'Please double check parameter values!';
+  }
+
+  /* check input file */
+  const errorLine = checkFileContent(fileContent);
+
+  if (errorLine) {
+    return 'Invalid input file format! (Line: ' + errorLine + ')';
+  }
+
+  return null;
+}
+
+function checkFileContent(fileContent) {
+  /* split by unix line ending convention */
+  const lines = fileContent.replace(/\r/g, '').split('\n');
+
+  /* check first line */
+  let numbers = lines[0].split(' ');
+
+  if (numbers.length != 3) {
+    return 1;
+  }
+
+  const vertexCount = Number(numbers[0]);
+  const edgeCount = Number(numbers[1]);
+  const fixedVertexes = Number(numbers[2]);
+
+  if (Number.isInteger(vertexCount) == false ||
+      Number.isInteger(edgeCount) == false ||
+      Number.isInteger(fixedVertexes) == false) {
+    return 1;
+  }
+
+  if (vertexCount < 1 || edgeCount < 0 || fixedVertexes < 0) {
+    return 1;
+  }
+
+  /* check every edge */
+  let lineIndex = 1;
+
+  for (lineIndex; lineIndex <= edgeCount; lineIndex += 1) {
+    if (lineIndex >= lines.length) {
+      return lineIndex + 1;
+    }
+    numbers = lines[lineIndex].split(' ');
+
+    if (numbers.length != 2) {
+      return lineIndex + 1;
+    }
+
+    const vertex1 = Number(numbers[0]);
+    const vertex2 = Number(numbers[1]);
+
+    if (Number.isInteger(vertex1) == false ||
+        Number.isInteger(vertex2) == false) {
+      return lineIndex + 1;
+    }
+
+    if (vertex1 < 1 || vertex1 > vertexCount ||
+        vertex2 < 1 || vertex2 > vertexCount) {
+      return lineIndex + 1;
+    }
+  }
+
+  /* check fixed vertexes */
+  const lastLineIndex = edgeCount + fixedVertexes;
+  
+  for (lineIndex; lineIndex <= lastLineIndex; lineIndex += 1) {
+    if (lineIndex >= lines.length) {
+      return lineIndex + 1;
+    }
+    numbers = lines[lineIndex].split(' ');
+
+    if (numbers.length != 2) {
+      return lineIndex + 1;
+    }
+
+    const vertex = Number(numbers[0]);
+    const label = Number(numbers[1]);
+
+    if (Number.isInteger(vertex) == false ||
+        Number.isInteger(label) == false) {
+      return lineIndex + 1;
+    }
+
+    if (vertex < 1 || vertex > vertexCount ||
+        label < 1 || label > maxLabel) {
+      return lineIndex + 1;
+    }
+  }
+
+  return null;
+}
--- src/components/algorithm/GeneticAlgorithm.vue
+++ src/components/algorithm/GeneticAlgorithm.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <a id="GA" />
 
     <md-tabs md-alignment="centered">
       <md-tab id="tab-general" md-label="Info">
@@ -16,12 +15,126 @@
       </md-tab>
     </md-tabs>
 
+    <a id="GA" />
+
+    <div class="md-layout">
+      <div class="md-layout-item md-size-33 padded">
+        <md-field>
+          <label>h</label>
+          <md-input v-model="parameters.h" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>k</label>
+          <md-input v-model="parameters.k" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Input File</label>
+          <md-file @change="fileChanged($event)" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Max. Label Value</label>
+          <md-input v-model="parameters.maxLabel" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Populations Count</label>
+          <md-input v-model="parameters.populationsCount" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Population Size</label>
+          <md-input v-model="parameters.populationSize" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Mutation Chance</label>
+          <md-input v-model="parameters.mutationChance" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Elites Count</label>
+          <md-input v-model="parameters.elitesCount" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Max. Generations</label>
+          <md-input v-model="parameters.maxGenerations" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <md-field>
+          <label>Max. Time (seconds)</label>
+          <md-input v-model="parameters.maxTime" type="number" :disabled="isRunning" />
+        </md-field>
+
+        <center>
+          <md-button
+            class="md-dense md-raised md-primary" v-on:click="start" :disabled="isRunning">
+            Start
+          </md-button>
+        </center>
+      </div>
+
+      <div class="md-layout-item md-size-33 padded-vertically">
+        <div id="chart"></div>
+        <div id="graph"></div>
+      </div>
+
+      <div class="md-layout-item md-size-33 padded">
+        <md-progress-spinner
+          v-if="isRunning"
+          class="center"
+          :md-diameter="100"
+          :md-stroke="10"
+          md-mode="indeterminate">
+        </md-progress-spinner>
+
+        <div v-if="!isRunning">
+          <div class="md-headline">Results</div> <br />
+
+          <span class="bold">Time Elapsed</span>
+          <span v-if="result.time">{{ result.time }} s</span>
+          <br/><br/>
+          <span class="bold">Iterations Done</span>
+          <span v-if="result.iterations">
+            {{ result.iterations }}
+            ({{Math.round((result.iterations / parameters.maxGenerations) * 100)}}%)
+          </span>
+          <br/><br/><br/>
+
+          <span class="bold">Solution</span>
+          <span>{{ result.solution }}</span>
+          <br/><br/>
+          <span class="bold">Is Solution Correct?</span>
+          <span v-bind:class="getResultColorClass()">{{ getResultCorrectness() }}</span>
+          <br/><br/>
+          <span class="bold">Number of Conflicting Vertexes</span>
+          <span>{{ result.conflictingVertexes }}</span>
+          <br/><br/>
+          <span class="bold">Chromatic Number</span>
+          <span v-if="result.chromaticNumber">&le; {{ result.chromaticNumber }}</span>
+          <br/><br/>
+          <span class="bold">Fitness</span>
+          <span>{{ result.fitness }}</span>
+          <br/><br/><br/>
+
+          <span class="error"> {{ result.errorMsg }} </span> <br/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { GoogleCharts } from 'google-charts';
+
+const svgPanZoom = require('svg-pan-zoom');
+const Viz = require('viz.js');
+
 export default {
-  name: 'GeneticAlgorithm',
+  name: 'SimulatedAnnealing',
   data() {
     return {
       info: {
@@ -35,7 +148,393 @@ export default {
                'torquatos vix, et has legere tamquam probatus. Quo ea erat interpretaris, ' +
                'nec modus tempor elaboraret in. Eu pri eros consul fastidii, unum facete no qui.',
       },
+
+      isRunning: false,
+      MAX_RUNNING_TIME: 60,
+
+      parameters: {
+        h: null,
+        k: null,
+        file: null,
+        maxLabel: null,
+        populationsCount: null,
+        populationSize: null,
+        mutationChance: null,
+        elitesCount: null,
+        maxGenerations: null,
+        maxTime: null,
+      },
+
+      result: {
+        time: null,
+        iterations: null,
+        solution: null,
+        isCorrect: null,
+        conflictingVertexes: null,
+        chromaticNumber: null,
+        fitness: null,
+        history: null,
+        errorMsg: null,
+      },
+
+      diagrams: {
+        chart: null,
+        chartData: null,
+        chartOptions: null,
+
+        svg: null,
+        panZoomTiger: null,
+      },
     };
+  },
+
+  mounted() {
+    // Load the charts library with a callback
+    /* GoogleCharts.load(this.drawChart);
+
+    window.addEventListener('resize', () => {
+      if (this.diagrams.chart) {
+        this.diagrams.chart.draw(this.diagrams.chartData, this.diagrams.chartOptions);
+      }
+    }); */
+  },
+
+  beforeUpdate() {
+    // this.drawChart();
+  },
+
+  methods: {
+    /* read data from loaded file */
+    fileChanged(event) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        this.parameters.file = e.target.result;
+      };
+
+      reader.readAsText(event.target.files[0]);
+    },
+
+    start() {
+      /* this.result.errorMsg = this.validateParameters();
+
+      if (this.result.errorMsg) {
+        return;
+      } */
+
+      this.isRunning = true;
+
+      const payload = {
+        h: parseInt(this.parameters.h, 10),
+        k: parseInt(this.parameters.k, 10),
+        fileContent: this.parameters.file,
+        maxLabel: parseInt(this.parameters.maxLabel, 10),
+        populationsCount: parseInt(this.parameters.populationsCount, 10),
+        populationSize: parseInt(this.parameters.populationSize, 10),
+        mutationChance: parseFloat(this.parameters.mutationChance, 10),
+        elitesCount: parseInt(this.parameters.elitesCount, 10),
+        maxGenerations: parseInt(this.parameters.maxGenerations, 10),
+        maxTime: parseInt(this.parameters.maxTime, 10),
+      };
+
+      fetch('https://graph-labeling.herokuapp.com/api/ga', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload) })
+        // eslint-disable-next-line
+        .then((res) => { return res.json(); })
+        .then((data) => {
+          if (!data.errorMsg) {
+            this.result = data;
+
+            // this.drawGraph();
+          }
+
+          this.isRunning = false;
+        });
+    },
+
+    drawChart() {
+      const list = [['Iteration', 'Temperature']];
+
+      if (this.parameters.temperature && this.parameters.coolingFactor &&
+          this.parameters.maxIterations &&
+          this.parameters.temperature > 0 &&
+          this.parameters.coolingFactor > 0 && this.parameters.coolingFactor < 1 &&
+          this.parameters.maxIterations > 0) {
+        let i;
+        for (i = 0; i < this.parameters.maxIterations;
+          i += Math.ceil(this.parameters.maxIterations / 100)) {
+          list.push([i, this.parameters.temperature * (this.parameters.coolingFactor ** i)]);
+        }
+      } else {
+        list.push([0, 0]);
+      }
+
+      this.diagrams.chartData = GoogleCharts.api.visualization.arrayToDataTable(list);
+
+      this.diagrams.chartOptions = {
+        title: '',
+        hAxis: { title: 'Iterations', viewWindow: { min: 0 } },
+        vAxis: { title: 'Temperature', viewWindow: { min: 0 } },
+        backgroundColor: { fill: 'transparent' },
+      };
+
+      if (!this.diagrams.chart) {
+        this.diagrams.chart = new GoogleCharts.api.visualization.AreaChart(document.getElementById('chart'));
+      }
+
+      this.diagrams.chart.draw(this.diagrams.chartData, this.diagrams.chartOptions);
+    },
+
+    drawGraph() {
+      if (!this.parameters.file || !this.result) {
+        return;
+      }
+
+      this.diagrams.svg = Viz(this.toDotFormat());
+      this.diagrams.svg = this.diagrams.svg.replace(/<svg width=".*?" height=".*?"/, '<svg width="1pt" height="1pt"');
+
+      const element = document.createElement('div');
+      element.style.width = '100%';
+      element.style.height = '100%';
+      element.innerHTML = this.diagrams.svg;
+
+      const svgElement = element.getElementsByTagName('svg')[0];
+      svgElement.id = 'svg';
+      svgElement.style.width = '100%';
+      svgElement.style.height = '100%';
+
+      const graphContainer = document.getElementById('graph');
+      graphContainer.innerHTML = '';
+      graphContainer.appendChild(element);
+
+      if (this.diagrams.panZoomTiger) {
+        // eslint-disable-next-line
+        this.diagrams.panZoomTiger.destroy;
+      }
+
+      // eslint-disable-next-line
+      this.diagrams.panZoomTiger = svgPanZoom('#svg', {
+        // viewportSelector: '.svg-pan-zoom_viewport',
+        panEnabled: true,
+        controlIconsEnabled: true,
+        zoomEnabled: true,
+        dblClickZoomEnabled: true,
+        mouseWheelZoomEnabled: true,
+        preventMouseEventsDefault: true,
+        zoomScaleSensitivity: 0.2,
+        minZoom: 0.5,
+        maxZoom: 10,
+        fit: true,
+        contain: false,
+        center: true,
+        refreshRate: 'auto',
+        beforeZoom: () => {},
+        onZoom: () => {},
+        beforePan: () => {},
+        onPan: () => {},
+        onUpdatedCTM: () => {},
+        // customEventsHandler: {},
+        eventsListenerElement: null,
+      });
+    },
+
+    toDotFormat() {
+      let graph = `graph {
+        bgcolor=transparent;`;
+
+      /* split by unix line ending convention */
+      const lines = this.parameters.file.replace(/\r/g, '').split('\n');
+
+      /* get number of edges */
+      let numbers = lines[0].split(' ');
+      const edgeCount = Number(numbers[1]);
+
+      /* get labeling reuslt */
+      const labels = this.result.solution.split(' ');
+      let i = 0;
+      for (i; i < labels.length; i += 1) {
+        graph += `${i + 1} [label="${labels[i]}"];`;
+      }
+
+      /* add every edge */
+      let lineIndex = 1;
+
+      for (lineIndex; lineIndex <= edgeCount; lineIndex += 1) {
+        numbers = lines[lineIndex].split(' ');
+
+        const vertex1 = Number(numbers[0]);
+        const vertex2 = Number(numbers[1]);
+
+        graph += `${vertex1} -- ${vertex2};`;
+      }
+
+      graph += '}';
+
+      return graph;
+    },
+
+    validateParameters() {
+      /* extract parameters */
+      const h = this.parameters.h;
+      const k = this.parameters.k;
+      const fileContent = this.parameters.file;
+      const maxLabel = this.parameters.maxLabel;
+      const temperature = this.parameters.temperature;
+      const coolingFactor = this.parameters.coolingFactor;
+      const maxIterations = this.parameters.maxIterations;
+      const maxTime = this.parameters.maxTime;
+
+      /* check parameters existance */
+      if (h == null ||
+          k == null ||
+          fileContent == null ||
+          maxLabel == null ||
+          temperature == null ||
+          coolingFactor == null ||
+          maxIterations == null ||
+          maxTime == null) {
+        return 'Please provide all parameters!';
+      }
+
+      /* check parameters type */
+      if (Number.isInteger(Number(h)) === false ||
+          Number.isInteger(Number(k)) === false ||
+          Number.isInteger(Number(maxLabel)) === false ||
+          isNaN(Number(temperature)) ||
+          isNaN(Number(coolingFactor)) ||
+          Number.isInteger(Number(maxIterations)) === false ||
+          Number.isInteger(Number(maxTime)) === false) {
+        return 'Please verify parameter types!';
+      }
+
+      /* check parameters value */
+      if (h < 0 ||
+          k < 0 ||
+          maxLabel < 1 ||
+          temperature <= 0 ||
+          coolingFactor <= 0 ||
+          coolingFactor >= 1 ||
+          maxIterations < 1 ||
+          maxTime < 1 ||
+          maxTime > this.MAX_RUNNING_TIME) {
+        return 'Please double check parameter values!';
+      }
+
+      /* check input file */
+      const errorLine = this.checkFileContent();
+
+      if (errorLine) {
+        return `Invalid input file format! (Line: ${errorLine})`;
+      }
+
+      return null;
+    },
+
+    checkFileContent() {
+      /* split by unix line ending convention */
+      const lines = this.parameters.file.replace(/\r/g, '').split('\n');
+
+      /* check first line */
+      let numbers = lines[0].split(' ');
+
+      if (numbers.length !== 3) {
+        return 1;
+      }
+
+      const vertexCount = Number(numbers[0]);
+      const edgeCount = Number(numbers[1]);
+      const fixedVertexes = Number(numbers[2]);
+
+      if (Number.isInteger(vertexCount) === false ||
+          Number.isInteger(edgeCount) === false ||
+          Number.isInteger(fixedVertexes) === false) {
+        return 1;
+      }
+
+      if (vertexCount < 1 || edgeCount < 0 || fixedVertexes < 0) {
+        return 1;
+      }
+
+      /* check every edge */
+      let lineIndex = 1;
+
+      for (lineIndex; lineIndex <= edgeCount; lineIndex += 1) {
+        if (lineIndex >= lines.length) {
+          return lineIndex + 1;
+        }
+        numbers = lines[lineIndex].split(' ');
+
+        if (numbers.length !== 2) {
+          return lineIndex + 1;
+        }
+
+        const vertex1 = Number(numbers[0]);
+        const vertex2 = Number(numbers[1]);
+
+        if (Number.isInteger(vertex1) === false ||
+            Number.isInteger(vertex2) === false) {
+          return lineIndex + 1;
+        }
+
+        if (vertex1 < 1 || vertex1 > vertexCount ||
+            vertex2 < 1 || vertex2 > vertexCount) {
+          return lineIndex + 1;
+        }
+      }
+
+      /* check fixed vertexes */
+      const lastLineIndex = edgeCount + fixedVertexes;
+
+      for (lineIndex; lineIndex <= lastLineIndex; lineIndex += 1) {
+        if (lineIndex >= lines.length) {
+          return lineIndex + 1;
+        }
+        numbers = lines[lineIndex].split(' ');
+
+        if (numbers.length !== 2) {
+          return lineIndex + 1;
+        }
+
+        const vertex = Number(numbers[0]);
+        const label = Number(numbers[1]);
+
+        if (Number.isInteger(vertex) === false ||
+            Number.isInteger(label) === false) {
+          return lineIndex + 1;
+        }
+
+        if (vertex < 1 || vertex > vertexCount ||
+            label < 1 || label > this.parameters.maxLabel) {
+          return lineIndex + 1;
+        }
+      }
+
+      return null;
+    },
+
+    getResultCorrectness() {
+      switch (this.result.isCorrect) {
+        case true:
+          return 'Yes';
+        case false:
+          return 'No';
+        default:
+          return '';
+      }
+    },
+
+    getResultColorClass() {
+      switch (this.result.isCorrect) {
+        case true:
+          return 'bold green';
+        case false:
+          return 'bold red';
+        default:
+          return '';
+      }
+    },
   },
 };
 </script>
@@ -52,5 +551,50 @@ export default {
     padding-right: 5%;
     padding-top: 5%;
     padding-bottom: 5%;
+  }
+  .padded {
+    padding-left: 5%;
+    padding-right: 5%;
+    padding-top: 3%;
+    padding-bottom: 3%;
+  }
+  .padded-vertically {
+    padding-top: 3%;
+    padding-bottom: 3%;
+  }
+  .center {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .bold {
+    font-weight: bold;
+  }
+  .error {
+    color: #F44336;
+    font-weight: bold;
+  }
+  #chart {
+    width: 100%;
+  }
+  #graph {
+    width: 100%;
+    height: 50%;
+    padding-top: 3%;
+  }
+  #svg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+  .red {
+    color: #F44336;
+  }
+  .green {
+    color: #8BC34A;
   }
 </style>
